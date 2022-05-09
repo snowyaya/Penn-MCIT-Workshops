65e47a4be11efcf3f225fd115a05af17dff29062
snowyaya <yayingliang@gmail.com>
2022-05-09 09:50:24 -0400

    Update preview

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -15,8 +15,8 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="MCIT Web3 Club" />
-        <meta name="author" content="MCIT Web3 Club" />
+        <meta name="description" content="Penn MCIT Workshops" />
+        <meta name="author" content="Penn MCIT Workshops" />
         <meta name="author" content="craftzdog" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
